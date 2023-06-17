15eaef90792acdc7c2df74352539c8e3e68aa1f5
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-17 10:40:36 +0300

    Import the getToDos hook and destructure in the TodoList component

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,9 +1,18 @@
+import { useGetTodosQuery } from '../api/apiSplice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState('');
+
+  const {
+    data: todos = [],
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
 
   const handleSubmit = (e) => {
     e.preventDefault();
