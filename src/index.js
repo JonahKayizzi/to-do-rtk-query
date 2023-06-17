88c36da8c38e7401bc83bf35503a381b9a1e947f
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-17 10:37:40 +0300

    Add the apiSlice to the index using ApiProvider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSplice } from './features/api/apiSplice';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSplice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
